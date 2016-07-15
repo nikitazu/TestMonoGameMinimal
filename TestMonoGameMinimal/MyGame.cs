57b4f04d903cf3140321c6f67e99ccce045d19d3
Nikita Zuev <nikitazu@gmail.com>
2016-07-15 08:24:47 +0300

    Noobs comments

--- TestMonoGameMinimal/MyGame.cs
+++ TestMonoGameMinimal/MyGame.cs
@@ -9,28 +9,43 @@ namespace TestMonoGameMinimal
         private readonly GraphicsDeviceManager _graphics;
         private SpriteBatch _sprites;
 
+        /// <summary>
+        /// Create a game.
+        /// </summary>
         public MyGame()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
 
+        /// <summary>
+        /// Prepair data before loading resources.
+        /// </summary>
         protected override void Initialize()
         {
             base.Initialize();
         }
 
+        /// <summary>
+        /// Load game resources.
+        /// </summary>
         protected override void LoadContent()
         {
             base.LoadContent();
             _sprites = new SpriteBatch(GraphicsDevice);
         }
 
+        /// <summary>
+        /// Unload game resources.
+        /// </summary>
         protected override void UnloadContent()
         {
             base.UnloadContent();
         }
 
+        /// <summary>
+        /// Update game state.
+        /// </summary>
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed
@@ -41,6 +56,9 @@ namespace TestMonoGameMinimal
             base.Update(gameTime);
         }
 
+        /// <summary>
+        /// Render game.
+        /// </summary>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
